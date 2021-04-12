eb948e9a307bdf0b4421651b1e44c9aab8d5bcb3
Lucila Rocha Correia <lucilarcorreia0312@gmail.com>
2021-04-11 22:30:29 -0300

    Jogo de cartas

---
+++ quinzenal3/blackjack/index.js
@@ -0,0 +1,8 @@
+console.log("Bem-Vindo ao jogo de BlackJack!");
+
+prompt("Quer iniciar uma nova rodada?");
+if (confirm("pergunta de sim ou não")) {
+  console.log("Ok, vamos começar");
+} else {
+  console.log("Acabou o jogo");
+}
